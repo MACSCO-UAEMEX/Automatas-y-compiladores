c350a4730c28b966886a6820bdfdaf08d030cb06
JohnRojas <ids_jonathan_rojas@hotmail.com>
2017-11-30 18:04:40 -0600

    Corrección de directorios
    Se corrigen algunos directorios para la ejecución del componente AFD_To_AFDM

--- Programa 3 - AFD to AFDM/Prueba_3.bat
+++ Programa 3 - AFD to AFDM/Prueba_3.bat
@@ -1,2 +1,3 @@
-java -jar "C:\Automatas\0003__AFD_To_AFDM\AFD_To_AFDM.jar" -CONFIG "C:\Automatas\0003__AFD_To_AFDM\0001__Entrada\Prueba_3\CONFIG.txt"
+java -jar AFD_To_AFDM.jar -CONFIG 0001__Entrada\Prueba_3\CONFIG.txt
+
 PAUSE
